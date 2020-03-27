60e4cfb64df0d05fbd59f2b845457cd43f76d1a9
Aman <sarcasaman@gmail.com>
2020-03-27 21:06:36 +0530

    fixed overflow issue

--- lib/widgets/scrolling_day_calendar_widget.dart
+++ lib/widgets/scrolling_day_calendar_widget.dart
@@ -41,74 +41,80 @@ class _ScrollingDayCalendarState extends State<ScrollingDayCalendar> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        MaterialButton(
-          onPressed: _selectedDate.day != widget.startDate.day
-              ? () {
-                  DateTime newDate = _selectedDate.subtract(
-                    Duration(days: 1),
-                  );
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 20.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          MaterialButton(
+            minWidth: 20,
+            onPressed: _selectedDate.day != widget.startDate.day
+                ? () {
+                    DateTime newDate = _selectedDate.subtract(
+                      Duration(days: 1),
+                    );
 
-                  setState(() {
-                    _selectedDate = newDate;
-                  });
+                    setState(() {
+                      _selectedDate = newDate;
+                    });
 
-                  widget.onDateChange(_selectedDate);
-                }
-              : null,
-          child: Icon(
-            Icons.arrow_back_ios,
-            color: _selectedDate.day != widget.startDate.day
-                ? Colors.black
-                : Colors.grey[300],
-            size: 12.0,
+                    widget.onDateChange(_selectedDate);
+                  }
+                : null,
+            child: Icon(
+              Icons.arrow_back_ios,
+              color: _selectedDate.day != widget.startDate.day
+                  ? Colors.black
+                  : Colors.grey[300],
+              size: 12.0,
+            ),
           ),
-        ),
-        SizedBox(width: 8.0),
-        Image(
-          width: 16.0,
-          height: 16.0,
-          image: AssetImage("images/ic_calendar.png"),
-        ),
-        SizedBox(width: 8.0),
-        Text(
-          DateFormat(widget.displayDateFormat != null
-                  ? widget.displayDateFormat
-                  : "dd/MM/yyyy")
-              .format(_selectedDate),
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-            color: Colors.black,
-            fontSize: 15.0,
+          SizedBox(width: 8.0),
+          Image(
+            width: 16.0,
+            height: 16.0,
+            image: AssetImage("images/ic_calendar.png"),
           ),
-        ),
-        SizedBox(width: 8.0),
-        MaterialButton(
-          onPressed: _selectedDate.day != widget.endDate.day
-              ? () {
-                  DateTime newDate = _selectedDate.add(
-                    Duration(days: 1),
-                  );
-
-                  setState(() {
-                    _selectedDate = newDate;
-                  });
-
-                  widget.onDateChange(_selectedDate);
-                }
-              : null,
-          child: Icon(
-            Icons.arrow_forward_ios,
-            color: _selectedDate.day != widget.endDate.day
-                ? Colors.black
-                : Colors.grey[300],
-            size: 12.0,
+          SizedBox(width: 8.0),
+          Text(
+            DateFormat(widget.displayDateFormat != null
+                    ? widget.displayDateFormat
+                    : "dd/MM/yyyy")
+                .format(_selectedDate),
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+              fontSize: 15.0,
+            ),
           ),
-        ),
-      ],
+          SizedBox(width: 8.0),
+          MaterialButton(
+            minWidth: 20,
+            onPressed: _selectedDate.day != widget.endDate.day
+                ? () {
+                    DateTime newDate = _selectedDate.add(
+                      Duration(days: 1),
+                    );
+
+                    setState(() {
+                      _selectedDate = newDate;
+                    });
+
+                    widget.onDateChange(_selectedDate);
+                  }
+                : null,
+            child: Icon(
+              Icons.arrow_forward_ios,
+              color: _selectedDate.day != widget.endDate.day
+                  ? Colors.black
+                  : Colors.grey[300],
+              size: 12.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
