a6311d615ddc7f7039dab3a22c834acd89909ebe
Aman <sarcasaman@gmail.com>
2020-05-06 14:24:23 +0530

    added payment confirm dialog, changed insuranceId to _id

--- lib/screens/payment/payments_methods.dart
+++ lib/screens/payment/payments_methods.dart
@@ -8,6 +8,7 @@ import 'package:hutano/widgets/fancy_button.dart';
 import 'package:hutano/widgets/inherited_widget.dart';
 import 'package:hutano/widgets/loading_background.dart';
 import 'package:hutano/widgets/widgets.dart';
+import 'package:intl/intl.dart';
 
 class PaymentMethodScreen extends StatefulWidget {
   PaymentMethodScreen({Key key}) : super(key: key);
@@ -22,11 +23,12 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
   Future<List<dynamic>> _insuranceFuture;
   ApiBaseHelper _api = ApiBaseHelper();
   InheritedContainerState _container;
-  String insuranceId;
+  String insuranceId, totalFee;
 
   bool _isLoading = false;
   Map appointmentData = new Map();
   Map providerMap;
+  String name, avatar, cashPayment = "0";
 
   @override
   void didChangeDependencies() {
@@ -40,6 +42,24 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
           appointmentData = providerMap["providerData"]["data"];
         } else {
           appointmentData = providerMap["providerData"];
+        }
+      }
+
+      if (providerMap["totalFee"] != null) {
+        totalFee = providerMap["totalFee"];
+      }
+
+      if (appointmentData["doctor"] != null) {
+        name = appointmentData["doctor"]["fullName"] ?? "---";
+        avatar = appointmentData["doctor"]["avatar"];
+      }
+    }
+
+    if (appointmentData["doctorData"] != null) {
+      for (dynamic detail in appointmentData["doctorData"]) {
+        if (detail["paymentMethod"] != null) {
+          cashPayment =
+              detail["paymentMethod"]["cashPayment"]?.toString() ?? "0";
         }
       }
     }
@@ -88,33 +108,37 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
                 width: MediaQuery.of(context).size.width - 76.0,
                 padding: const EdgeInsets.only(right: 20.0, left: 40.0),
                 child: FancyButton(
-                  title: "Continue",
+                  title: "Pay \$$totalFee",
                   onPressed: () {
-                    _loading(true);
-                    Map map = new Map();
+                    if (_radioValue != null) {
+                      _loading(true);
+                      Map map = new Map();
 
-                    if (_radioValue == 3) {
-                      map["cashPayment"] = "1";
-                    } else if (_radioValue == 1) {
-                    } else {
-                      map["insuranceId"] = insuranceId;
-                    }
-                    SharedPref().getToken().then((token) {
-                      _api
-                          .postPayment(
-                              token,
-                              _container.appointmentIdMap["appointmentId"]
-                                  .toString(),
-                              map)
-                          .then((value) {
-                        _loading(false);
-                        Widgets.showToast("Payment success");
-                      }).futureError((error) {
-                        _loading(false);
-                        Widgets.showToast(error.toString());
-                        error.toString().debugLog();
+                      if (_radioValue == 3) {
+                        map["cashPayment"] = "1";
+                      } else if (_radioValue == 1) {
+                      } else {
+                        map["insuranceId"] = insuranceId;
+                      }
+                      SharedPref().getToken().then((token) {
+                        _api
+                            .postPayment(
+                                token,
+                                _container.appointmentIdMap["appointmentId"]
+                                    .toString(),
+                                map)
+                            .then((value) {
+                          _loading(false);
+                          showThankDialog();
+                        }).futureError((error) {
+                          _loading(false);
+                          Widgets.showToast(error.toString());
+                          error.toString().debugLog();
+                        });
                       });
-                    });
+                    } else {
+                      Widgets.showToast("Please select a payment method");
+                    }
                   },
                 ),
               ),
@@ -248,7 +272,7 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
                                   ? null
                                   : (int value) {
                                       setState(() => _radioValue = value);
-                                      insuranceId = data[index]["insuranceId"];
+                                      insuranceId = data[index]["_id"];
                                     },
                             ),
                           ),
@@ -320,10 +344,10 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
                 value: value,
                 groupValue: _radioValue,
                 materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                onChanged: (title.toLowerCase().contains("cash") &&
-                        appointmentData["cashPayment"] == 1)
-                    ? _handleRadioValueChange
-                    : null,
+                onChanged:
+                    (title.toLowerCase().contains("cash") && cashPayment == "1")
+                        ? _handleRadioValueChange
+                        : null,
                 //TODO: check for cardPayment when it's done in API
               ),
             ),
@@ -351,6 +375,106 @@ class _PaymentMethodScreenState extends State<PaymentMethodScreen> {
           color: Colors.black.withOpacity(0.93),
         ),
       ),
+    );
+  }
+
+  void showThankDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(14.0),
+            ),
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                14.0,
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                "ic_feedback_done".imageIcon(
+                  width: 80,
+                  height: 80,
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Payment Completed!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black.withOpacity(0.90),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "\$$totalFee Paid on " +
+                      DateFormat("dd").format(DateTime.now()) +
+                      "th " +
+                      DateFormat("MMMM").format(DateTime.now()),
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 26),
+                ClipOval(
+                  child: avatar == null
+                      ? "profile_user".imageIcon(
+                          height: 38,
+                          width: 38,
+                        )
+                      : Image.network(
+                          ApiBaseHelper.imageUrl + avatar,
+                          height: 38,
+                          width: 38,
+                        ),
+                ),
+                SizedBox(height: 6),
+                Text(
+                  name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black.withOpacity(0.85),
+                  ),
+                ),
+                SizedBox(height: 26),
+                FlatButton(
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    color: AppColors.goldenTainoi,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                    child: Text(
+                      "Done",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    onPressed: () {
+                      FocusScope.of(context).requestFocus(FocusNode());
+
+                      int count = 0;
+                      Navigator.of(context).popUntil((_) => count++ >= 2);
+                    })
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
