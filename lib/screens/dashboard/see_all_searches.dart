81f69e01814207a8a48c90abeb2a1b225fdd1b7a
Aman <sarcasaman@gmail.com>
2020-05-13 17:19:51 +0530

    added professionalTitle

--- lib/screens/dashboard/see_all_searches.dart
+++ lib/screens/dashboard/see_all_searches.dart
@@ -28,9 +28,9 @@ class _SeeAllSearchScreeenState extends State<SeeAllSearchScreeen> {
   Widget build(BuildContext context) {
     List<dynamic> _list = widget.arguments.list;
     String title = widget.arguments.title;
-    int type = widget.arguments.type;
+    String professionalTitle = "---";
 
-    debugPrint("List: ${_list.toString()}", wrapWidth: 1024);
+    int type = widget.arguments.type;
 
     return Scaffold(
       backgroundColor: AppColors.goldenTainoi,
@@ -38,47 +38,57 @@ class _SeeAllSearchScreeenState extends State<SeeAllSearchScreeen> {
         title: title,
         color: Colors.white,
         isAddBack: true,
-        padding: const EdgeInsets.all(0.0),
+        padding: EdgeInsets.zero,
         child: ListView.separated(
-            separatorBuilder: (BuildContext context, int index) => Divider(),
-            physics: ClampingScrollPhysics(),
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(20.0),
-            itemCount: _list.length,
-            itemBuilder: (context, index) {
-              if (_list.isNotEmpty) {
-                return title.toLowerCase().contains("provider")
-                    ? ProviderTileWidget(
-                        name: _list[index]["fullName"],
-                        profession: _list[index]["fullName"],
-                        onTap: () {
-                          _container
-                              .setProviderId(_list[index]["_id"].toString());
-                          Navigator.of(context)
-                              .pushNamed(Routes.providerProfileScreen);
-                        },
-                      )
-                    : ListTile(
-                        title: Text(_list[index]["title"]),
-                        onTap: () {
-                          _container.getProjectsResponse().clear();
-
-                          if (type == 1) {
-                            _container.setProjectsResponse(
-                                "specialityId", _list[index]["_id"]);
-                          } else if (type == 3) {
-                            _container.setProjectsResponse(
-                                "serviceId", _list[index]["_id"]);
-                          }
-
-                          Navigator.of(context)
-                              .pushNamed(Routes.providerListScreen);
-                        },
-                      );
+          separatorBuilder: (BuildContext context, int index) => Divider(),
+          physics: ClampingScrollPhysics(),
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20.0),
+          itemCount: _list.length,
+          itemBuilder: (context, index) {
+            if (_list.isNotEmpty) {
+              if (_list[index]["UserDoctorDetails"] != null) {
+                dynamic details = _list[index]["UserDoctorDetails"];
+                if (details["professionalTitle"] != null) {
+                  professionalTitle =
+                      details["professionalTitle"]["title"]?.toString() ??
+                          "---";
+                }
               }
 
-              return Container();
-            }),
+              return title.toLowerCase().contains("provider")
+                  ? ProviderTileWidget(
+                      name: _list[index]["fullName"],
+                      profession: professionalTitle,
+                      onTap: () {
+                        _container
+                            .setProviderId(_list[index]["_id"].toString());
+                        Navigator.of(context)
+                            .pushNamed(Routes.providerProfileScreen);
+                      },
+                    )
+                  : ListTile(
+                      title: Text(_list[index]["title"]),
+                      onTap: () {
+                        _container.getProjectsResponse().clear();
+
+                        if (type == 1) {
+                          _container.setProjectsResponse(
+                              "specialityId", _list[index]["_id"]);
+                        } else if (type == 3) {
+                          _container.setProjectsResponse(
+                              "serviceId", _list[index]["_id"]);
+                        }
+
+                        Navigator.of(context)
+                            .pushNamed(Routes.providerListScreen);
+                      },
+                    );
+            }
+
+            return Container();
+          },
+        ),
       ),
     );
   }
