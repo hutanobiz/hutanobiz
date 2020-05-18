984f5b6b065dca735c7ddbe972542ae4cdf2301a
Aman <sarcasaman@gmail.com>
2020-05-18 18:51:21 +0530

    ProviderFiltersScreen: in progress

--- lib/screens/dashboard/provider_filters.dart
+++ lib/screens/dashboard/provider_filters.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hutano/api/api_helper.dart';
 import 'package:hutano/colors.dart';
+import 'package:hutano/models/filter_radio_model.dart';
 import 'package:hutano/models/medicalHistory.dart';
 import 'package:hutano/utils/shared_prefrences.dart';
 import 'package:hutano/widgets/inherited_widget.dart';
 import 'package:hutano/widgets/loading_background.dart';
-import 'package:hutano/widgets/vertical_tabs.dart';
+import 'package:hutano/utils/extensions.dart';
 
 class ProviderFiltersScreen extends StatefulWidget {
   ProviderFiltersScreen({Key key}) : super(key: key);
@@ -21,9 +22,19 @@ class _ProviderFiltersScreenState extends State<ProviderFiltersScreen> {
   InheritedContainerState _container;
   List<dynamic> _diseaseList = List();
 
+  List<RadioModel> filtersList = new List<RadioModel>();
+
   @override
   void initState() {
     _todoFuture = api.getDiseases();
+
+    filtersList.add(RadioModel(false, "Professional Title"));
+    filtersList.add(RadioModel(false, "Speciality"));
+    filtersList.add(RadioModel(false, "Degree"));
+    filtersList.add(RadioModel(false, "Experience"));
+    filtersList.add(RadioModel(false, "Appointment Type"));
+    filtersList.add(RadioModel(false, "Distance"));
+    filtersList.add(RadioModel(false, "Language"));
 
     SharedPref().getToken().then((token) {
       api.getLastAppointmentDetails(token).then((response) {
@@ -54,67 +65,90 @@ class _ProviderFiltersScreenState extends State<ProviderFiltersScreen> {
       body: LoadingBackground(
         title: "Filters",
         isAddBack: false,
-        // onForwardTap: () {
-        //   if (_diseaseList.length > 0) {
-        //     _container.setConsentToTreatData("medicalHistory", _diseaseList);
-        //   }
-
-        //   Navigator.of(context).pushNamed(Routes.seekingCureScreen);
-        // },
         color: Colors.white,
         padding: EdgeInsets.zero,
-        child: VerticalTabs(
-          tabsWidth: 150,
-          tabs: <Tab>[
-            Tab(child: Text('Professional Title')),
-            Tab(child: Text('Dart')),
-            Tab(child: Text('NodeJS')),
-            Tab(child: Text('PHP')),
-            Tab(child: Text('HTML 5')),
-          ],
-          contents: <Widget>[
-            Container(child: Text('Flutter'), padding: EdgeInsets.all(20)),
-            Container(child: Text('Dart'), padding: EdgeInsets.all(20)),
-            Container(child: Text('NodeJS'), padding: EdgeInsets.all(20)),
-            Container(child: Text('PHP'), padding: EdgeInsets.all(20)),
-            Container(child: Text('HTML 5'), padding: EdgeInsets.all(20))
+        child: Row(
+          children: <Widget>[
+            Material(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFF3F2F8).withOpacity(0.50),
+                  borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(22.0),
+                  ),
+                ),
+                width: 140,
+                child: ListView.builder(
+                  itemCount: filtersList.length,
+                  itemBuilder: (context, index) {
+                    RadioModel model = filtersList[index];
+                    return filterWidget(model).onClick(onTap: () {
+                      setState(() {
+                        filtersList
+                            .forEach((element) => element.isSelected = false);
+                        model.isSelected = true;
+                      });
+                    });
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder<List<MedicalHistory>>(
+                future: _todoFuture,
+                builder: (_, snapshot) {
+                  if (snapshot.hasData) {
+                    List<MedicalHistory> data = snapshot.data;
+
+                    return ListView.builder(
+                      padding: const EdgeInsets.all(0.0),
+                      shrinkWrap: true,
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        MedicalHistory medicalHistory = data[index];
+
+                        return CheckboxListTile(
+                          title: Text(medicalHistory.name),
+                          value: _diseaseList.contains(medicalHistory.sId),
+                          activeColor: AppColors.goldenTainoi,
+                          onChanged: (value) {
+                            setState(() {
+                              value == true
+                                  ? _diseaseList.add(medicalHistory.sId)
+                                  : _diseaseList.remove(medicalHistory.sId);
+                            });
+                          },
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
           ],
         ),
-        // FutureBuilder<List<MedicalHistory>>(
-        //   future: _todoFuture,
-        //   builder: (_, snapshot) {
-        //     if (snapshot.hasData) {
-        //       List<MedicalHistory> data = snapshot.data;
+      ),
+    );
+  }
 
-        //       return ListView.builder(
-        //         padding: const EdgeInsets.all(0.0),
-        //         shrinkWrap: true,
-        //         itemCount: data.length,
-        //         itemBuilder: (context, index) {
-        //           MedicalHistory medicalHistory = data[index];
-
-        //           return CheckboxListTile(
-        //             title: Text(medicalHistory.name),
-        //             value: _diseaseList.contains(medicalHistory.sId),
-        //             activeColor: AppColors.goldenTainoi,
-        //             onChanged: (value) {
-        //               setState(() {
-        //                 value == true
-        //                     ? _diseaseList.add(medicalHistory.sId)
-        //                     : _diseaseList.remove(medicalHistory.sId);
-        //               });
-        //             },
-        //           );
-        //         },
-        //       );
-        //     } else if (snapshot.hasError) {
-        //       return Text("${snapshot.error}");
-        //     }
-        //     return Center(
-        //       child: CircularProgressIndicator(),
-        //     );
-        //   },
-        // ),
+  Widget filterWidget(RadioModel model) {
+    return Container(
+      height: 70,
+      padding: EdgeInsets.all(20),
+      child: Text(model.text),
+      decoration: BoxDecoration(
+        color: model.isSelected ? Colors.white : Colors.transparent,
+        borderRadius: model.isSelected
+            ? BorderRadius.only(
+                bottomRight: const Radius.circular(22.0),
+                topRight: const Radius.circular(22.0),
+              )
+            : BorderRadius.all(Radius.zero),
       ),
     );
   }
--- lib/screens/dashboard/provider_list_screen.dart
+++ lib/screens/dashboard/provider_list_screen.dart
@@ -74,45 +74,54 @@ class _ProviderListScreenState extends State<ProviderListScreen> {
   }
 
   Widget searchBar() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Row(
       children: <Widget>[
-        TextFormField(
-          key: _searchKey,
-          maxLines: 1,
-          keyboardType: TextInputType.text,
-          onChanged: (value) {
-            setState(() {
-              _searchText = value;
-            });
-            filterSearch(value);
-          },
-          decoration: InputDecoration(
-            floatingLabelBehavior: FloatingLabelBehavior.never,
-            filled: true,
-            fillColor: Colors.white,
-            labelStyle: TextStyle(fontSize: 13.0, color: Colors.grey),
-            labelText: "Search for general medicine",
-            suffixIcon: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Image(
-                width: 34.0,
-                height: 34.0,
-                image: AssetImage("images/ic_search.png"),
+        Expanded(
+          child: TextFormField(
+            key: _searchKey,
+            maxLines: 1,
+            keyboardType: TextInputType.text,
+            onChanged: (value) {
+              setState(() {
+                _searchText = value;
+              });
+              filterSearch(value);
+            },
+            decoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              filled: true,
+              fillColor: Colors.white,
+              labelStyle: TextStyle(fontSize: 13.0, color: Colors.grey),
+              labelText: "Search for general medicine",
+              suffixIcon: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Image(
+                  width: 34.0,
+                  height: 34.0,
+                  image: AssetImage("images/ic_search.png"),
+                ),
               ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              contentPadding: EdgeInsets.fromLTRB(12.0, 15.0, 14.0, 14.0),
             ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            contentPadding: EdgeInsets.fromLTRB(12.0, 15.0, 14.0, 14.0),
           ),
         ),
+        InkWell(
+          onTap: () =>
+              Navigator.of(context).pushNamed(Routes.providerFiltersScreen),
+          child: Image(
+            width: 34.0,
+            height: 34.0,
+            image: AssetImage("images/ic_search.png"),
+          ),
+        )
       ],
     );
   }
