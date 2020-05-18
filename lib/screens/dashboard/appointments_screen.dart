7c9f6ef347d3e8eab885360491819a0dd05e720d
Aman <sarcasaman@gmail.com>
2020-05-18 19:40:51 +0530

    refreshing appointment list after doing review or payment

--- lib/screens/dashboard/appointments_screen.dart
+++ lib/screens/dashboard/appointments_screen.dart
@@ -243,213 +243,234 @@ class _AppointmentsScreenState extends State<AppointmentsScreen> {
         borderRadius: BorderRadius.circular(14.0),
         border: Border.all(color: Colors.grey[300]),
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(left: 14, right: 14.0),
-            child: Stack(
-              children: <Widget>[
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+      child: Material(
+        type: MaterialType.transparency,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(14.0),
+          splashColor: Colors.grey[200],
+          onTap: () {
+            _container.setAppointmentId(response["_id"].toString());
+            Navigator.of(context)
+                .pushNamed(Routes.appointmentDetailScreen)
+                .whenComplete(() => appointmentsFuture());
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 14, right: 14.0),
+                child: Stack(
                   children: <Widget>[
-                    Container(
-                      width: 62.0,
-                      height: 62.0,
-                      margin: const EdgeInsets.only(top: 14.0),
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: avatar == null
-                              ? AssetImage('images/profile_user.png')
-                              : NetworkImage(ApiBaseHelper.imageUrl + avatar),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                        border: Border.all(
-                          color: Colors.grey[300],
-                          width: 1.0,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 14.0, left: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "$name",
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w700,
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 62.0,
+                          height: 62.0,
+                          margin: const EdgeInsets.only(top: 14.0),
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: avatar == null
+                                  ? AssetImage('images/profile_user.png')
+                                  : NetworkImage(
+                                      ApiBaseHelper.imageUrl + avatar),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            border: Border.all(
+                              color: Colors.grey[300],
+                              width: 1.0,
                             ),
                           ),
-                          SizedBox(height: 5.0),
-                          Row(
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 14.0, left: 10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
-                              Image.asset(
-                                "images/ic_star_rating.png",
-                                width: 12,
-                                height: 12,
-                                color: AppColors.goldenTainoi,
-                              ),
-                              SizedBox(width: 4.0),
                               Text(
-                                "$averageRating \u2022 $professionalTitle",
+                                "$name",
+                                overflow: TextOverflow.ellipsis,
                                 style: TextStyle(
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black.withOpacity(0.7),
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w700,
                                 ),
+                              ),
+                              SizedBox(height: 5.0),
+                              Row(
+                                children: <Widget>[
+                                  Image.asset(
+                                    "images/ic_star_rating.png",
+                                    width: 12,
+                                    height: 12,
+                                    color: AppColors.goldenTainoi,
+                                  ),
+                                  SizedBox(width: 4.0),
+                                  Text(
+                                    "$averageRating \u2022 $professionalTitle",
+                                    style: TextStyle(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black.withOpacity(0.7),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 4.0,
                               ),
                             ],
                           ),
-                          SizedBox(
-                            height: 4.0,
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: listType == 1 ? 25.0 : 20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              appointmentType,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12.0,
+                              ),
+                            ),
                           ),
+                          SizedBox(height: 13.0),
+                          listType == 1
+                              ? Container()
+                              : Align(
+                                  alignment: Alignment.centerRight,
+                                  child: status?.appointmentStatus(),
+                                ),
                         ],
                       ),
                     ),
                   ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: listType == 1 ? 25.0 : 20.0),
-                  child: Column(
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          appointmentType,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 12.0,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 13.0),
-                      listType == 1
-                          ? Container()
-                          : Align(
-                              alignment: Alignment.centerRight,
-                              child: status?.appointmentStatus(),
-                            ),
-                    ],
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 3.0, 8.0, 3.0),
+                child: Divider(
+                  color: Colors.grey[300],
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 3.0, 8.0, 3.0),
-            child: Divider(
-              color: Colors.grey[300],
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 12.0, right: 12.0, bottom: 10.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Row(
-                    children: <Widget>[
-                      "ic_location_grey".imageIcon(width: 11.0),
-                      SizedBox(width: 3.0),
-                      Expanded(
-                        child: Text(
-                          "$address",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 12.0, right: 12.0, bottom: 10.0),
+                child: Row(
                   children: <Widget>[
-                    "ic_app_distance".imageIcon(),
-                    SizedBox(width: 5.0),
-                    Text(
-                      "$distance miles",
-                      style: TextStyle(
-                        color: AppColors.windsor,
+                    Expanded(
+                      child: Row(
+                        children: <Widget>[
+                          "ic_location_grey".imageIcon(width: 11.0),
+                          SizedBox(width: 3.0),
+                          Expanded(
+                            child: Text(
+                              "$address",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        "ic_app_distance".imageIcon(),
+                        SizedBox(width: 5.0),
+                        Text(
+                          "$distance miles",
+                          style: TextStyle(
+                            color: AppColors.windsor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 12.0, right: 12.0, bottom: 14.0),
+                child: Row(
+                  children: <Widget>[
+                    "ic_appointment_time".imageIcon(height: 12.0, width: 12.0),
+                    SizedBox(width: 3.0),
+                    Expanded(
+                      child: Text(
+                        (response['date'] != null
+                                ? DateFormat('EEEE, dd MMMM, ')
+                                    .format(DateTime.parse(response['date']))
+                                    .toString()
+                                : "---") +
+                            (response["fromTime"] != null
+                                ? response["fromTime"]
+                                    .toString()
+                                    .timeOfDay(context)
+                                : "---") +
+                            " - " +
+                            (response["toTime"] != null
+                                ? response["toTime"]
+                                    .toString()
+                                    .timeOfDay(context)
+                                : "---"),
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 12.0, right: 12.0, bottom: 14.0),
-            child: Row(
-              children: <Widget>[
-                "ic_appointment_time".imageIcon(height: 12.0, width: 12.0),
-                SizedBox(width: 3.0),
-                Expanded(
-                  child: Text(
-                    (response['date'] != null
-                            ? DateFormat('EEEE, dd MMMM, ')
-                                .format(DateTime.parse(response['date']))
-                                .toString()
-                            : "---") +
-                        (response["fromTime"] != null
-                            ? response["fromTime"].toString().timeOfDay(context)
-                            : "---") +
-                        " - " +
-                        (response["toTime"] != null
-                            ? response["toTime"].toString().timeOfDay(context)
-                            : "---"),
-                    style: TextStyle(
-                      color: Colors.black.withOpacity(0.5),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              listType == 1 && paymentType == 0
-                  ? rightButton(listType, "Confirm Payment", () {
-                      _container.setProviderData("providerData", response);
-                      _container.setAppointmentId(response["_id"].toString());
-                      _container.setProviderData("totalFee", null);
-                      Navigator.of(context).pushNamed(
-                        Routes.paymentMethodScreen,
-                        arguments: true,
-                      );
-                    })
-                  : listType == 2 && status == "4"
-                      ? leftButton(userRating, "Treatment summary", () {
+              ),
+              Row(
+                children: <Widget>[
+                  listType == 1 && paymentType == 0
+                      ? rightButton(listType, "Confirm Payment", () {
                           _container.setProviderData("providerData", response);
-                          Navigator.of(context).pushNamed(
-                              Routes.treatmentSummaryScreen,
-                              arguments: response);
+                          _container
+                              .setAppointmentId(response["_id"].toString());
+                          _container.setProviderData("totalFee", null);
+                          Navigator.of(context)
+                              .pushNamed(
+                                Routes.paymentMethodScreen,
+                                arguments: true,
+                              )
+                              .whenComplete(() => appointmentsFuture());
+                        })
+                      : listType == 2 && status == "4"
+                          ? leftButton(userRating, "Treatment summary", () {
+                              _container.setProviderData(
+                                  "providerData", response);
+                              Navigator.of(context).pushNamed(
+                                  Routes.treatmentSummaryScreen,
+                                  arguments: response);
+                            })
+                          : Container(),
+                  listType == 2 && status == "4" && userRating == null
+                      ? rightButton(listType, "Rate Now", () {
+                          _container.setProviderData("providerData", response);
+                          _container
+                              .setAppointmentId(response["_id"].toString());
+                          Navigator.of(context)
+                              .pushNamed(
+                                Routes.rateDoctorScreen,
+                                arguments: false,
+                              )
+                              .whenComplete(() => appointmentsFuture());
                         })
                       : Container(),
-              listType == 2 && status == "4" && userRating == null
-                  ? rightButton(listType, "Rate Now", () {
-                      _container.setProviderData("providerData", response);
-                      _container.setAppointmentId(response["_id"].toString());
-                      Navigator.of(context)
-                          .pushNamed(
-                            Routes.rateDoctorScreen,
-                            arguments: false,
-                          )
-                          .whenComplete(() => appointmentsFuture());
-                    })
-                  : Container(),
+                ],
+              ),
             ],
           ),
-        ],
-      ).onClick(
-        context: context,
-        routeName: Routes.appointmentDetailScreen,
-        onTap: () => _container.setAppointmentId(response["_id"].toString()),
+        ),
       ),
     );
   }
