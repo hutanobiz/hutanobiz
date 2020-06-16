4ef0d7ce1171b367090da0c716f74a9947b732ed
Aman Gupta <aman@appeing.xyz>
2020-06-16 14:21:57 +0530

    removed unnecessary padding widget

--- lib/widgets/review_widget.dart
+++ lib/widgets/review_widget.dart
@@ -18,101 +18,98 @@ class ReviewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                width: 58.0,
-                height: 58.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: avatar == null
-                        ? AssetImage('images/profile_user.png')
-                        : NetworkImage(ApiBaseHelper.imageUrl + avatar),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: new BorderRadius.all(Radius.circular(50.0)),
-                  border: new Border.all(
-                    color: Colors.grey[300],
-                    width: 1.0,
-                  ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              width: 58.0,
+              height: 58.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: avatar == null
+                      ? AssetImage('images/profile_user.png')
+                      : NetworkImage(ApiBaseHelper.imageUrl + avatar),
+                  fit: BoxFit.cover,
                 ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        reviewerName,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Text(
-                          reviewDate,
-                          style: TextStyle(
-                            fontSize: 12.0,
-                            color: Colors.black.withOpacity(0.85),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                borderRadius: new BorderRadius.all(Radius.circular(50.0)),
+                border: new Border.all(
+                  color: Colors.grey[300],
+                  width: 1.0,
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  RatingBar(
-                    initialRating: reviewerRating,
-                    itemSize: 20.0,
-                    minRating: 1,
-                    direction: Axis.horizontal,
-                    allowHalfRating: true,
-                    itemCount: 5,
-                    glow: false,
-                    itemBuilder: (context, index) => Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                    ),
-                    onRatingUpdate: null,
-                  ),
-                  Text(
-                    reviewerRating.toString(),
-                    style: TextStyle(
-                      fontSize: 13.0,
-                      color: Colors.black87,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: Text(
-              reviewText,
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.85),
               ),
             ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      reviewerName,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text(
+                        reviewDate,
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.black.withOpacity(0.85),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                RatingBar(
+                  initialRating: reviewerRating,
+                  itemSize: 20.0,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  glow: false,
+                  itemBuilder: (context, index) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: null,
+                ),
+                Text(
+                  reviewerRating.toString(),
+                  style: TextStyle(
+                    fontSize: 13.0,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: Text(
+            reviewText,
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.85),
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
