fddf7aa711c11a24ea18d52e89efd21ff1d7c04c
Aman Gupta <aman@appeing.xyz>
2020-07-03 11:06:38 +0530

    MedicalHistoryScreen: added text field on selecting other disease, implemented delete medical history api

--- lib/screens/appointments/medical_history.dart
+++ lib/screens/appointments/medical_history.dart
@@ -30,9 +30,15 @@ class _MedicalHistoryScreenState extends State<MedicalHistoryScreen> {
 
   bool _isLoading = false;
 
+  TextEditingController _otherDiseaseController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
+
+    _otherDiseaseController.addListener(() {
+      setState(() {});
+    });
 
     if (widget.isBottomButtonsShow != null) {
       isBottomButtonsShow = widget.isBottomButtonsShow;
@@ -63,9 +69,15 @@ class _MedicalHistoryScreenState extends State<MedicalHistoryScreen> {
   }
 
   @override
+  void dispose() {
+    _otherDiseaseController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: true,
       backgroundColor:
           isBottomButtonsShow ? AppColors.goldenTainoi : Colors.white,
       body: LoadingBackground(
@@ -77,58 +89,101 @@ class _MedicalHistoryScreenState extends State<MedicalHistoryScreen> {
         onForwardTap: saveMedicalHistory,
         color: Colors.white,
         padding: const EdgeInsets.all(5.0),
-        child: Stack(
+        child: Column(
           children: <Widget>[
-            FutureBuilder<List<MedicalHistory>>(
-              future: _medicalFuture,
-              builder: (_, snapshot) {
-                if (snapshot.hasData) {
-                  List<MedicalHistory> data = snapshot.data;
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                physics: ClampingScrollPhysics(),
+                children: <Widget>[
+                  FutureBuilder<List<MedicalHistory>>(
+                    future: _medicalFuture,
+                    builder: (_, snapshot) {
+                      if (snapshot.hasData) {
+                        List<MedicalHistory> data = snapshot.data;
 
-                  return ListView.builder(
-                    padding: const EdgeInsets.only(bottom: 65),
-                    shrinkWrap: true,
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
-                      MedicalHistory medicalHistory = data[index];
+                        return ListView.builder(
+                          padding: EdgeInsets.only(
+                              bottom:
+                                  _diseaseList.contains('Others') ? 10 : 65),
+                          shrinkWrap: true,
+                          physics: ClampingScrollPhysics(),
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            MedicalHistory medicalHistory = data[index];
 
-                      return CheckboxListTile(
-                        title: Text(medicalHistory.name),
-                        value: _diseaseList.contains(medicalHistory.name),
-                        activeColor: AppColors.goldenTainoi,
-                        onChanged: (value) {
-                          setState(() {
-                            if (value) {
-                              if (!_diseaseList.contains(medicalHistory.name)) {
-                                _diseaseList.add(medicalHistory.name);
-                              }
-                            } else {
-                              _diseaseList.remove(medicalHistory.name);
-                            }
-                          });
-                        },
+                            return CheckboxListTile(
+                              title: Text(medicalHistory.name),
+                              value: _diseaseList.contains(medicalHistory.name),
+                              activeColor: AppColors.goldenTainoi,
+                              onChanged: (value) {
+                                setState(() {
+                                  if (value) {
+                                    if (!_diseaseList
+                                        .contains(medicalHistory.name)) {
+                                      _diseaseList.add(medicalHistory.name);
+                                    }
+                                  } else {
+                                    setLoading(true);
+                                    api
+                                        .deletePatientMedicalHistory(
+                                            token, medicalHistory.name)
+                                        .then((value) {
+                                      setLoading(false);
+                                      _diseaseList.remove(medicalHistory.name);
+                                    }).futureError((error) {
+                                      setLoading(false);
+                                      error.toString().debugLog();
+                                    });
+                                  }
+                                });
+                              },
+                            );
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
+                      return Center(
+                        child: CircularProgressIndicator(),
                       );
                     },
-                  );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
+                  ),
+                  _diseaseList.contains('Others')
+                      ? Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                          child: TextField(
+                            controller: _otherDiseaseController,
+                            keyboardType: TextInputType.text,
+                            maxLines: 1,
+                            textInputAction: TextInputAction.done,
+                            decoration: InputDecoration(
+                              labelStyle: TextStyle(color: Colors.grey),
+                              labelText: "Type your disease...",
+                              alignLabelWithHint: true,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey[300]),
+                                borderRadius: BorderRadius.circular(14.0),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(14.0),
+                              ),
+                            ),
+                          ),
+                        )
+                      : Container(),
+                ],
+              ),
             ),
             isBottomButtonsShow
                 ? Container()
-                : Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      height: 55,
-                      margin: const EdgeInsets.fromLTRB(20, 0, 20, 10),
-                      child: FancyButton(
-                        title: 'Save',
-                        onPressed: saveMedicalHistory,
-                      ),
+                : Container(
+                    height: 55,
+                    margin: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+                    child: FancyButton(
+                      title: 'Save',
+                      onPressed: saveMedicalHistory,
                     ),
                   ),
           ],
@@ -139,6 +194,10 @@ class _MedicalHistoryScreenState extends State<MedicalHistoryScreen> {
 
   void saveMedicalHistory() {
     Map diseaseMap = {};
+
+    if (_otherDiseaseController.text.isNotEmpty) {
+      _diseaseList.add(_otherDiseaseController.text);
+    }
 
     if (_diseaseList.length > 0) {
       setLoading(true);
