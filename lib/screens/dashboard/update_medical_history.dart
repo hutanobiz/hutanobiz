551f722fa1ac73153342cc7bdedb825993511e6e
Aman Gupta <aman@appening.xyz>
2020-07-07 11:31:47 +0530

    UpdateMedicalHistory: changed tabs design

--- lib/screens/dashboard/update_medical_history.dart
+++ lib/screens/dashboard/update_medical_history.dart
@@ -3,6 +3,7 @@ import 'package:hutano/colors.dart';
 import 'package:hutano/screens/appointments/medical_history.dart';
 import 'package:hutano/screens/appointments/upload_documents.dart';
 import 'package:hutano/screens/appointments/upload_images.dart';
+import 'package:hutano/widgets/loading_background.dart';
 
 class UpdateMedicalHistory extends StatefulWidget {
   @override
@@ -29,71 +30,85 @@ class _UpdateMedicalHistoryState extends State<UpdateMedicalHistory>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Row(
-          children: <Widget>[
-            InkWell(
-              customBorder: CircleBorder(),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Icon(
-                  Icons.arrow_back_ios,
-                  size: 15.0,
-                  color: Colors.black,
+      backgroundColor: AppColors.goldenTainoi,
+      body: LoadingBackground(
+        title: 'Medical History',
+        isAddBack: true,
+        addBackButton: false,
+        color: Colors.white,
+        padding: const EdgeInsets.only(top: 0.0, bottom: 10.0),
+        child: tabBarWidget(),
+      ),
+    );
+  }
+
+  Widget tabBarWidget() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: new Container(
+            height: 44,
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(14.0),
+                border: Border.all(color: Colors.grey[300])),
+            child: TabBar(
+              controller: _controller,
+              labelPadding: EdgeInsets.all(0),
+              unselectedLabelColor: AppColors.windsor,
+              labelColor: Colors.white,
+              indicatorColor: Colors.white,
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                color: AppColors.windsor,
+              ),
+              tabs: [
+                Container(
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Medical History',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
                 ),
-              ),
-              onTap: () => Navigator.pop(context),
-            ),
-            SizedBox(width: 10),
-            Text(
-              'Medical History',
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-        bottom: TabBar(
-          controller: _controller,
-          labelColor: Colors.black,
-          unselectedLabelColor: Colors.black.withOpacity(0.4),
-          indicator: UnderlineTabIndicator(
-            insets: EdgeInsets.symmetric(horizontal: 10.0),
-            borderSide: BorderSide(
-              color: AppColors.windsor,
-              width: 2.0,
-              style: BorderStyle.solid,
+                Container(
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Medical Images',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+                  ),
+                ),
+                Container(
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Medical Documents',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          tabs: [
-            Tab(
-              child: Text(
-                'Medical History',
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Tab(
-              child: Text(
-                'Medical Images',
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Tab(
-              child: Text(
-                'Medical Documents',
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
         ),
-      ),
-      body: TabBarView(
-        controller: _controller,
-        children: _children,
-      ),
+        Expanded(
+          child: TabBarView(
+            controller: _controller,
+            children: _children,
+          ),
+        ),
+      ],
     );
   }
 }
