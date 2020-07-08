47016e8e508ea169c7568bb3e583b4bbfe9fa6b7
Aman Gupta <aman@appening.xyz>
2020-07-08 11:44:30 +0530

    Widgets: added error and confirmation dialogs

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:hutano/colors.dart';
 
 class Widgets {
   static void showSnackBar(
@@ -17,6 +20,133 @@ class Widgets {
       toastLength: Toast.LENGTH_LONG,
       gravity: ToastGravity.BOTTOM,
       backgroundColor: Colors.indigo,
+    );
+  }
+
+  static void showErrorDialog({
+    @required BuildContext context,
+    String title,
+    @required String description,
+    String buttonText,
+    Function onPressed,
+  }) {
+    showDialog(
+      context: context,
+      builder: (context) => Platform.isIOS
+          ? CupertinoAlertDialog(
+              title: Text(title ?? "Alert"),
+              content: Text(description),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  onPressed: onPressed ??
+                      () {
+                        Navigator.of(context).pop();
+                      },
+                  isDefaultAction: true,
+                  child: Text(
+                    buttonText ?? "Ok",
+                    style: TextStyle(
+                      color: AppColors.windsor,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : AlertDialog(
+              title: Text(title ?? "Error"),
+              content: Text(description),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: onPressed ??
+                      () {
+                        Navigator.of(context).pop();
+                      },
+                  child: Text(
+                    buttonText ?? "Ok",
+                    style: TextStyle(
+                      color: AppColors.windsor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+
+  static void showConfirmationDialog({
+    @required BuildContext context,
+    String title,
+    @required String description,
+    String leftText,
+    Function onLeftPressed,
+    String rightText,
+    Function onRightPressed,
+  }) {
+    showDialog(
+      context: context,
+      builder: (context) => Platform.isIOS
+          ? CupertinoAlertDialog(
+              title: Text(title ?? ""),
+              content: Text(description),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  onPressed: onLeftPressed ??
+                      () {
+                        Navigator.of(context).pop();
+                      },
+                  isDefaultAction: true,
+                  child: Text(
+                    leftText ?? "Yes",
+                    style: TextStyle(
+                      color: AppColors.windsor,
+                    ),
+                  ),
+                ),
+                CupertinoDialogAction(
+                  onPressed: onRightPressed ??
+                      () {
+                        Navigator.of(context).pop();
+                      },
+                  isDefaultAction: true,
+                  child: Text(
+                    rightText ?? "No",
+                    style: TextStyle(
+                      color: AppColors.windsor,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : AlertDialog(
+              title: Text(title ?? "Are You Sure"),
+              content: Text(description),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: onLeftPressed ??
+                      () {
+                        Navigator.of(context).pop();
+                      },
+                  child: Text(
+                    leftText ?? "Yes",
+                    style: TextStyle(
+                      color: AppColors.windsor,
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: onRightPressed ??
+                      () {
+                        Navigator.of(context).pop();
+                      },
+                  child: Text(
+                    rightText ?? "No",
+                    style: TextStyle(
+                      color: AppColors.windsor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
     );
   }
 
