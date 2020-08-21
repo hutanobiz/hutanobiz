b2b7cda9725cd7506b438a06fc40894b6977fc2c
Aman Gupta <aman@appening.xyz>
2020-08-21 19:04:28 +0530

    added CustomTextField widget

---
+++ lib/widgets/textform_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  CustomTextField(
+      {Key key,
+      @required this.labelText,
+      this.inputType,
+      this.initialValue,
+      this.maxLength,
+      this.controller,
+      @required this.onChanged,
+      this.autoFocus,
+      this.autoValidate,
+      this.validator})
+      : super(key: key);
+
+  final String labelText;
+  final String initialValue;
+  final TextInputType inputType;
+  final int maxLength;
+  final Function onChanged;
+  final TextEditingController controller;
+  final bool autoFocus;
+  final bool autoValidate;
+  final FormFieldValidator<String> validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: key,
+      initialValue: initialValue ?? null,
+      autofocus: autoFocus ?? false,
+      autovalidate: autoValidate ?? false,
+      validator: validator ?? null,
+      onChanged: onChanged,
+      controller: controller ?? null,
+      maxLength: maxLength ?? null,
+      decoration: InputDecoration(
+        labelText: labelText,
+        counterText: "",
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey[300]),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+      ),
+      keyboardType: inputType ?? TextInputType.text,
+    );
+  }
+}
