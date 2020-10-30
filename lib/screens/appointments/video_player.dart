9617a7410a959aa7668fcba53b2ddfc4e32ee561
amit <amit@appening.xyz>
2020-10-30 14:12:20 +0530

    video origial path added

--- lib/screens/appointments/video_player.dart
+++ lib/screens/appointments/video_player.dart
@@ -17,8 +17,8 @@ class _SamplePlayerState extends State<SamplePlayer> {
     super.initState();
     flickManager = FlickManager(
       videoPlayerController:
-          VideoPlayerController.network('https://hutano-video.s3-us-west-1.amazonaws.com/directory1/directory2/dd07c3a99c42ab3a30d8ec880d3a74e4_5f9b8507724c2711aea6554f.m3u8'),
-    );
+          VideoPlayerController.network(widget.videoPath),
+           );
   }
 
   @override
