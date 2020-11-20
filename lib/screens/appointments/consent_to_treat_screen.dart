a1f9e122f1138db3c800ff9b50dd86bc9a9f53e6
amit <amit@appening.xyz>
2020-11-20 18:45:37 +0530

    consent to treat text from Api

--- lib/screens/appointments/consent_to_treat_screen.dart
+++ lib/screens/appointments/consent_to_treat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hutano/api/api_helper.dart';
 import 'package:hutano/colors.dart';
 import 'package:hutano/routes.dart';
 import 'package:hutano/utils/shared_prefrences.dart';
@@ -17,6 +18,23 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
   bool isAgree = false;
   String _name = "";
 
+  dynamic _responseData;
+  ApiBaseHelper api = ApiBaseHelper();
+  Future<dynamic> _requestsConsent;
+  dynamic response;
+  bool isInitialLoad = true;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    SharedPref().getToken().then((token) {
+      setState(() {
+        _requestsConsent = api.getConsentContent(token);
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,8 +50,30 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
               : Widgets.showToast("Please agree to continue");
         },
         color: Colors.white,
-        child: ListView(
-          children: widgetList(),
+        child: FutureBuilder<dynamic>(
+          future: _requestsConsent,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              if (isInitialLoad) {
+                response = snapshot.data;
+                isInitialLoad = false;
+              }
+              if (response is String) {
+                return Center(child: Text(response));
+              } else {
+                _responseData = response;
+                return ListView(
+                  children: widgetList(),
+                );
+              }
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       ),
     );
@@ -44,13 +84,16 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
         .getValue("fullName")
         .then((value) => setState(() => _name = value ?? "---"));
 
+    String text =
+        _responseData['textString'].toString().replaceAll("\$_name", _name);
     List<Widget> formWidget = new List();
 
     formWidget.add(Column(
       children: <Widget>[],
     ));
     formWidget.add(Text(
-      "This is to certify that I, $_name, as an adult participant give my consent to a provider on Hutano’s platform and its medical representative to provide medical care for the medical condition stated here. I certify that all the information provided here is true and provided by me or a representative under my direction. I certify that I am at least 18 years of age. I understand and agree that I am financially responsible for the care provided by the Hutano provider. I understand that medical services provided by the may qualify for insurance reimbursement. I understand that it will be my responsibility and provider to file insurance claims with my insurance company. I agree to have Hutano LLC charge my credit card on behalf of the provider for services that are not covered by my insurance provider for the amount agreed upon.",
+      text,
+      // "This is to certify that I, $_name, as an adult participant give my consent to a provider on Hutano’s platform and its medical representative to provide medical care for the medical condition stated here. I certify that all the information provided here is true and provided by me or a representative under my direction. I certify that I am at least 18 years of age. I understand and agree that I am financially responsible for the care provided by the Hutano provider. I understand that medical services provided by the may qualify for insurance reimbursement. I understand that it will be my responsibility and provider to file insurance claims with my insurance company. I agree to have Hutano LLC charge my credit card on behalf of the provider for services that are not covered by my insurance provider for the amount agreed upon.",
       style: TextStyle(
         fontSize: 14.0,
       ),
