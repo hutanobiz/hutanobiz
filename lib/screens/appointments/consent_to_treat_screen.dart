602bb7982b98ac891f4cc4ea3092797a74002d50
Guru Patel <guruaj.patel.sa@gmail.com>
2020-12-31 12:29:52 +0530

    [fix] consent screen issue

--- lib/screens/appointments/consent_to_treat_screen.dart
+++ lib/screens/appointments/consent_to_treat_screen.dart
@@ -22,14 +22,14 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
   ApiBaseHelper api = ApiBaseHelper();
   Future<dynamic> _requestsConsent;
   dynamic response;
-  bool isInitialLoad = true;
+  bool isInitialLoad = false;
   bool isLoading = false;
 
   @override
   void initState() {
     SharedPref().getToken().then((token) {
       setState(() {
-        _requestsConsent = api.getConsentContent(token);
+        // _requestsConsent = api.getConsentContent(token);
       });
     });
     super.initState();
@@ -53,26 +53,29 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
         child: FutureBuilder<dynamic>(
           future: _requestsConsent,
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              if (isInitialLoad) {
-                response = snapshot.data;
-                isInitialLoad = false;
-              }
-              if (response is String) {
-                return Center(child: Text(response));
-              } else {
-                _responseData = response;
-                return ListView(
+            return ListView(
                   children: widgetList(),
                 );
-              }
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+            // if (snapshot.hasData) {
+            //   if (isInitialLoad) {
+            //     response = snapshot.data;
+            //     isInitialLoad = false;
+            //   }
+            //   if (response is String) {
+            //     return Center(child: Text(response));
+            //   } else {
+            //     _responseData = response;
+            //     return ListView(
+            //       children: widgetList(),
+            //     );
+            //   }
+            // } else if (snapshot.hasError) {
+            //   return Text("${snapshot.error}");
+            // } else {
+            //   return Center(
+            //     child: CircularProgressIndicator(),
+            //   );
+            // }
           },
         ),
       ),
@@ -84,16 +87,16 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
         .getValue("fullName")
         .then((value) => setState(() => _name = value ?? "---"));
 
-    String text =
-        _responseData['textString'].toString().replaceAll("\$_name", _name);
+    // String text =
+    //     _responseData['textString'].toString().replaceAll("\$_name", _name);
     List<Widget> formWidget = new List();
 
     formWidget.add(Column(
       children: <Widget>[],
     ));
     formWidget.add(Text(
-      text,
-      // "This is to certify that I, $_name, as an adult participant give my consent to a provider on Hutano’s platform and its medical representative to provide medical care for the medical condition stated here. I certify that all the information provided here is true and provided by me or a representative under my direction. I certify that I am at least 18 years of age. I understand and agree that I am financially responsible for the care provided by the Hutano provider. I understand that medical services provided by the may qualify for insurance reimbursement. I understand that it will be my responsibility and provider to file insurance claims with my insurance company. I agree to have Hutano LLC charge my credit card on behalf of the provider for services that are not covered by my insurance provider for the amount agreed upon.",
+      // text,
+      "This is to certify that I, $_name, as an adult participant give my consent to a provider on Hutano’s platform and its medical representative to provide medical care for the medical condition stated here. I certify that all the information provided here is true and provided by me or a representative under my direction. I certify that I am at least 18 years of age. I understand and agree that I am financially responsible for the care provided by the Hutano provider. I understand that medical services provided by the may qualify for insurance reimbursement. I understand that it will be my responsibility and provider to file insurance claims with my insurance company. I agree to have Hutano LLC charge my credit card on behalf of the provider for services that are not covered by my insurance provider for the amount agreed upon.",
       style: TextStyle(
         fontSize: 14.0,
       ),
