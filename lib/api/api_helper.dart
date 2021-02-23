967f40bf8ca70ed4b3872cca2e1453c4b762c7a6
Guru Patel <gurupatel107@gmail.com>
2021-02-23 10:19:45 +0530

    [refactor] removed unnecessary code

--- lib/api/api_helper.dart
+++ lib/api/api_helper.dart
@@ -17,14 +17,8 @@ import '../routes.dart';
 
 class ApiBaseHelper {
   NetworkUtil _netUtil = new NetworkUtil();
-  // static const String base_url = "https://staging.hutano.xyz/";
   // static const String base_url = "https://sa-staging.hutano.xyz/";
-  // static const String base_url = "https://api.stage.hutano.com/";
   static const String base_url = "https://api.stage.hutano.com/";
-  // static const String base_url = "https://api.stage.hutano.com/";
-  
-  
-  // static const String base_url = "http://2edb7d715014.ngrok.io/";
   static const String imageUrl = base_url + "uploads/";
 
   Future<dynamic> login(Map loginData) {
--- lib/src/utils/app_config.dart
+++ lib/src/utils/app_config.dart
@@ -22,16 +22,12 @@ void setEnvironment(Environment env) {
 
 dynamic get apiBaseUrl {
   // _config = devConstants;
-  // return "http://2edb7d715014.ngrok.io/";
   return "https://api.stage.hutano.com/";
 }
 
 dynamic get imageUrl {
   // return "https://api.dev.hutano.com/";
     return "https://api.stage.hutano.com/";
-    // return "https://api.stage.hutano.com/";
-    // return "http://4725d5c73be1.ngrok.io/";
-    // return "http://2edb7d715014.ngrok.io/";
 }
 
 dynamic get googleApiKey {
