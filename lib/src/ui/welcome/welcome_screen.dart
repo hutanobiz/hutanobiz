61c7d5745860b45efbbd02dd47f21fad6ff3c09b
Guru Patel <gururaj.patel.sa@gmail.com>
2021-03-18 15:16:31 +0530

    [refactor] welcome screen ui change

--- lib/src/ui/welcome/welcome_screen.dart
+++ lib/src/ui/welcome/welcome_screen.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:hutano/routes.dart';
-import 'package:hutano/src/apis/api_manager.dart';
-import 'package:hutano/src/apis/error_model.dart';
-import 'package:hutano/src/ui/registration_steps/email_verification/model/req_email.dart';
-import 'package:hutano/src/utils/navigation.dart';
-import 'package:hutano/src/utils/preference_key.dart';
-import 'package:hutano/src/utils/preference_utils.dart';
-import 'package:hutano/src/widgets/toast.dart';
-import '../../utils/constants/constants.dart';
+
+import '../../../routes.dart';
+import '../../apis/api_manager.dart';
+import '../../apis/error_model.dart';
 import '../../utils/color_utils.dart';
+import '../../utils/constants/constants.dart';
 import '../../utils/constants/file_constants.dart';
 import '../../utils/dimens.dart';
 import '../../utils/localization/localization.dart';
+import '../../utils/preference_key.dart';
+import '../../utils/preference_utils.dart';
 import '../../widgets/hutano_button.dart';
-import '../../widgets/hutano_header.dart';
 import '../../widgets/hutano_header_info.dart';
 import '../../widgets/hutano_steps_header.dart';
+import '../../widgets/skip_later.dart';
+import '../../widgets/toast.dart';
+import '../registration_steps/email_verification/model/req_email.dart';
 
 class WelcomeScreen extends StatefulWidget {
   @override
@@ -29,89 +29,98 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       color: Colors.white,
       child: SafeArea(
         child: Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                HutanoHeader(
-                  headerInfo: HutanoHeaderInfo(
-                    showLogo: true,
-                    title: Localization.of(context).welcome,
-                    subTitle: Localization.of(context).accNowActive,
-                    subTitleFontSize: fontSize15,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(height: spacing30),
+              HutanoHeaderInfo(
+                showLogo: true,
+                title: Localization.of(context).welcome,
+                subTitle: Localization.of(context).accNowActive,
+                subTitleFontSize: fontSize15,
+              ),
+              SizedBox(height: spacing30),
+              Row(
+                children: [
+                  SizedBox(width: 35),
+                  Image.asset(
+                    FileConstants.icEdit,
+                    height: 25,
+                    width: 25,
                   ),
-                  spacing: spacing20,
-                ),
-                SizedBox(height:spacing30),
-                Row(
-                  children: [
-                    SizedBox(width:35),
-                    Image.asset(
-                      FileConstants.icEdit,
-                      height: 25,
-                      width: 25,
+                  SizedBox(width: 20),
+                  Flexible(
+                    child: Text(
+                      Localization.of(context).completeTask,
+                      maxLines: 2,
+                      style: const TextStyle(
+                          color: colorBlack2,
+                          fontFamily: gilroySemiBold,
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 13),
                     ),
-                    SizedBox(width:20),
-                    Flexible(
-                      child: Text(
-                        Localization.of(context).completeTask,
-                        maxLines: 2,
-                        style: const TextStyle(
-                            color: colorLightBlack2,
-                            fontWeight: fontWeightSemiBold,
-                            fontSize: fontSize15),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: spacing20,
-                ),
-                HutanoStepsHeader(
-                  title: Localization.of(context).activateEmail,
-                  subTitle: Localization.of(context).activateEmailDesc,
-                  iconText: stepOne,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: spacing20,
+              ),
+              HutanoStepsHeader(
+                title: Localization.of(context).activateEmail,
+                subTitle: Localization.of(context).activateEmailDesc,
+                iconText: stepOne,
+                titleStyle: _getTitletStyle(),
+                subTitleStyle: _getSubTitletStyle(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              HutanoStepsHeader(
+                title: Localization.of(context).addPaymentOption,
+                subTitle: Localization.of(context).addPaymentDesc,
+                iconText: stepTwo,
+                titleStyle: _getTitletStyle(),
+                subTitleStyle: _getSubTitletStyle(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              HutanoStepsHeader(
+                  title: Localization.of(context).addFamily,
+                  subTitle: Localization.of(context).addFamilyDesc,
                   titleStyle: _getTitletStyle(),
                   subTitleStyle: _getSubTitletStyle(),
+                  iconText: stepThree),
+              SizedBox(
+                height: 20,
+              ),
+              HutanoStepsHeader(
+                title: Localization.of(context).addProvider,
+                subTitle: Localization.of(context).addProviderDesc,
+                titleStyle: _getTitletStyle(),
+                subTitleStyle: _getSubTitletStyle(),
+                iconText: stepFour,
+              ),
+              SizedBox(
+                height: spacing20,
+              ),
+              Spacer(),
+              _buildCompleteTaskNowButton(context),
+              SizedBox(
+                height: spacing20,
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: SkipLater(
+                  onTap: _skipTaskNow,
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                HutanoStepsHeader(
-                  title: Localization.of(context).addPaymentOption,
-                  subTitle: Localization.of(context).addPaymentDesc,
-                  iconText: stepTwo,
-                  titleStyle: _getTitletStyle(),
-                  subTitleStyle: _getSubTitletStyle(),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                HutanoStepsHeader(
-                    title: Localization.of(context).addFamily,
-                    subTitle: Localization.of(context).addFamilyDesc,
-                    titleStyle: _getTitletStyle(),
-                    subTitleStyle: _getSubTitletStyle(),
-                    iconText: stepThree),
-                SizedBox(
-                  height: 20,
-                ),
-                HutanoStepsHeader(
-                  title: Localization.of(context).addProvider,
-                  subTitle: Localization.of(context).addProviderDesc,
-                  titleStyle: _getTitletStyle(),
-                  subTitleStyle: _getSubTitletStyle(),
-                  iconText: stepFour,
-                ),
-                SizedBox(
-                  height: spacing20,
-                ),
-                _buildCompleteTaskNowButton(context),
-                SizedBox(
-                  height: spacing20,
-                ),
-                _buildSkipTaskNowButton(context),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
           ),
         ),
       ),
@@ -120,23 +129,30 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
   _getTitletStyle() {
     return TextStyle(
-        color: colorBlack, fontWeight: fontWeightMedium, fontSize: fontSize15);
+      color: colorBlack2,
+      fontSize: fontSize15,
+      fontWeight: FontWeight.w500,
+      fontFamily: gilroyMedium,
+      fontStyle: FontStyle.normal,
+    );
   }
 
   _getSubTitletStyle() {
     return TextStyle(
-        color: colorBlack85,
-        fontWeight: fontWeightRegular,
+        fontFamily: gilroyRegular,
+        fontWeight: FontWeight.w400,
+        fontStyle: FontStyle.normal,
+        color: colorBlack2.withOpacity(0.85),
         fontSize: fontSize14);
   }
 
   _buildCompleteTaskNowButton(BuildContext context) => Padding(
       padding: const EdgeInsets.only(left: spacing20, right: spacing20),
       child: HutanoButton(
-        buttonType: HutanoButtonType.withIcon,
+        buttonType: HutanoButtonType.withPrefixIcon,
         isIconButton: true,
         icon: FileConstants.icDone,
-        color: colorYellow,
+        color: colorOrange,
         iconSize: spacing30,
         label: Localization.of(context).completeTaskNow,
         onPressed: _completeTaskNow,
@@ -151,13 +167,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         labelColor: colorBlack,
         iconSize: spacing20,
         color: Colors.transparent,
-        icon: FileConstants.icSkipLater,
+        icon: FileConstants.icSkipBlack,
         label: Localization.of(context).skipTasks,
         onPressed: _skipTaskNow,
       ));
 
   _completeTaskNow() {
-    Navigator.of(context).pushReplacementNamed( routeEmailVerification);
+    Navigator.of(context).pushReplacementNamed(routeEmailVerification);
     setBool(PreferenceKey.perFormedSteps, true);
     _sendCode();
   }
@@ -175,7 +191,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   }
 
   _skipTaskNow() {
-    Navigator.of(context).pushNamedAndRemoveUntil( Routes.dashboardScreen ,(route) => false);
     setBool(PreferenceKey.skipStep, true);
+    Navigator.of(context)
+        .pushNamedAndRemoveUntil(Routes.dashboardScreen, (route) => false);
   }
 }
