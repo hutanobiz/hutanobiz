27e9ae048e05f267708f63a7003d990de43debf5
Guru Patel <gururaj.patel.sa@gmail.com>
2021-04-06 14:45:51 +0530

    [add] branch io config

--- android/app/src/main/kotlin/xyz/appening/hutano/MainActivity.kt
+++ android/app/src/main/kotlin/xyz/appening/hutano/MainActivity.kt
@@ -7,7 +7,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 
-class MainActivity : FlutterFragmentActivity() {
+class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
