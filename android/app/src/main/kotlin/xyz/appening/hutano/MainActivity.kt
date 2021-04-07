a02ac7f726c3452bc0a27e2207b03bef72004f21
Guru Patel <gururaj.patel.sa@gmail.com>
2021-04-07 14:48:59 +0530

    [fix] local auth and branch io activity issue

--- android/app/src/main/kotlin/xyz/appening/hutano/MainActivity.kt
+++ android/app/src/main/kotlin/xyz/appening/hutano/MainActivity.kt
@@ -7,7 +7,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
