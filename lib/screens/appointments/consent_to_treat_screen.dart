1ab93001cba41ddb9b542a70674f918a6923a54c
amit <amit@appening.xyz>
2021-08-17 17:09:10 +0530

    consent to treat fixed

--- lib/screens/appointments/consent_to_treat_screen.dart
+++ lib/screens/appointments/consent_to_treat_screen.dart
@@ -3,7 +3,10 @@ import 'package:hutano/apis/api_helper.dart';
 import 'package:hutano/colors.dart';
 import 'package:hutano/dimens.dart';
 import 'package:hutano/routes.dart';
+import 'package:hutano/utils/color_utils.dart';
+import 'package:hutano/utils/localization/localization.dart';
 import 'package:hutano/utils/shared_prefrences.dart';
+import 'package:hutano/widgets/inherited_widget.dart';
 import 'package:hutano/widgets/loading_background_new.dart';
 import 'package:hutano/widgets/round_corner_checkbox.dart';
 import 'package:hutano/widgets/widgets.dart';
@@ -23,20 +26,53 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
   ApiBaseHelper api = ApiBaseHelper();
   Future<dynamic> _requestsConsent;
   dynamic response;
-  bool isInitialLoad = true;
+  bool isInitialLoad = false;
   bool isLoading = false;
+  String userId;
+  String name, nameTitle;
 
   @override
   void initState() {
     SharedPref().getToken().then((token) {
       setState(() {
-        _requestsConsent = api.getConsentContent(token);
+        // _requestsConsent = api.getConsentContent(token);
       });
     });
     SharedPref()
         .getValue("fullName")
         .then((value) => setState(() => _name = value ?? "---"));
     super.initState();
+  }
+
+  Future<void> didChangeDependencies() async {
+    super.didChangeDependencies();
+
+    var _container = InheritedContainer.of(context);
+    userId = _container.providerResponse['providerData']['_id'];
+
+    if (_container.providerResponse['providerData']["userId"] != null &&
+        _container.providerResponse['providerData']["userId"] is Map) {
+      nameTitle = _container.providerResponse['providerData']["userId"]["title"]
+              ?.toString() ??
+          'Dr. ';
+      name = nameTitle +
+              _container.providerResponse['providerData']["userId"]["fullName"]
+                  ?.toString() ??
+          "---";
+    } else if (_container.providerResponse['providerData']['data'][0]["User"] !=
+        null is Map) {
+      nameTitle = (_container.providerResponse['providerData']['data'][0]
+                  ["User"][0]["title"]
+              ?.toString() ??
+          'Dr. ');
+      name = '$nameTitle ' +
+          (_container.providerResponse['providerData']['data'][0]["User"][0]
+                      ["fullName"]
+                  ?.toString() ??
+              "---");
+    }
+
+    print(_container);
   }
 
   @override
@@ -69,29 +105,29 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
         child: FutureBuilder<dynamic>(
           future: _requestsConsent,
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              if (isInitialLoad) {
-                response = snapshot.data;
-                isInitialLoad = false;
-              }
-              if (response is String) {
-                return ListView(
-                  children: widgetList(),
-                );
-                //Center(child: Text(response));
-              } else {
-                _responseData = response;
-                return ListView(
-                  children: widgetList(),
-                );
-              }
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+            return ListView(
+              children: widgetList(),
+            );
+            // if (snapshot.hasData) {
+            //   if (isInitialLoad) {
+            //     response = snapshot.data;
+            //     isInitialLoad = false;
+            //   }
+            //   if (response is String) {
+            //     return Center(child: Text(response));
+            //   } else {
+            //     _responseData = response;
+            //     return ListView(
+            //       children: widgetList(),
+            //     );
+            //   }
+            // } else if (snapshot.hasError) {
+            //   return Text("${snapshot.error}");
+            // } else {
+            //   return Center(
+            //     child: CircularProgressIndicator(),
+            //   );
+            // }
           },
         ),
       ),
@@ -106,27 +142,37 @@ class _ConsentToTreatScreenState extends State<ConsentToTreatScreen> {
     formWidget.add(Column(
       children: <Widget>[],
     ));
+    formWidget.add(_consentToTreatHeader(context));
     formWidget.add(Text(
-      // text,
-      "This is to certify that I, $_name, as an adult participant give my consent to a provider on Hutano’s platform and its medical representative to provide medical care for the medical condition stated here. I certify that all the information provided here is true and provided by me or a representative under my direction. I certify that I am at least 18 years of age. I understand and agree that I am financially responsible for the care provided by the Hutano provider. I understand that medical services provided by the may qualify for insurance reimbursement. I understand that it will be my responsibility and provider to file insurance claims with my insurance company. I agree to have Hutano LLC charge my credit card on behalf of the provider for services that are not covered by my insurance provider for the amount agreed upon.",
-      style: TextStyle(
-        height: 1.5,
-        fontSize: 14.0,
-      ),
-      textAlign: TextAlign.justify,
-    ));
+        "I, $_name, certify that I am an adult over the age of 18 and as such, do hereby willingly agree to receive medical care by $name. I understand and agree that I will participate in my treatment plan and I may discontinue treatment and/or withdraw my consent for treatment at any time. \n\nI voluntarily consent to the rendering of healthcare including diagnostic procedures, and medical treatment by the provider and/or the provider's authorized designees. \n\nIn the event that I am injured or ill while under the care of the provider, I hereby give permission to the provider to provide first aid and take appropriate measures including contacting Emergency Medical Services (EMS) and arranging transportation to the nearest emergency medical facility.  ",
+        style: TextStyle(
+          color: colorBlack2,
+          fontSize: fontSize14,
+          fontWeight: fontWeightRegular,
+          letterSpacing: -0.14,
+        )));
 
     formWidget.add(SizedBox(height: 45));
 
     formWidget.add(Center(
       child: RoundCornerCheckBox(
-        title: "I consent to treatment",
+        title: Localization.of(context).iAgreeLabel,
         value: isAgree,
         onCheck: (value) => setState(() => isAgree = !isAgree),
       ),
     ));
-    formWidget.add(SizedBox(height: 60));
 
     return formWidget;
   }
+
+  Widget _consentToTreatHeader(BuildContext context) => Padding(
+        padding: EdgeInsets.symmetric(vertical: spacing20),
+        child: Text(
+          Localization.of(context).consentToTreatHeader,
+          style: TextStyle(
+              color: Color(0xff0e1c2a),
+              fontSize: fontSize16,
+              fontWeight: fontWeightBold),
+        ),
+      );
 }
