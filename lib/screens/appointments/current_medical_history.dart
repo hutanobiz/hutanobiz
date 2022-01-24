210b3ae6769dd5a646fdca6ba35c197c58f1fda8
amit <amit@appening.xyz>
2022-01-24 17:15:10 +0530

    recreationalDrugs web app fix

--- lib/screens/appointments/current_medical_history.dart
+++ lib/screens/appointments/current_medical_history.dart
@@ -185,7 +185,7 @@ class _CurrentAppointmentMedicalHistoryState
                       physics: NeverScrollableScrollPhysics(),
                       itemCount: socialHistory['recreationalDrugs'].length,
                       itemBuilder: (context, index) {
-                        return Text(
+                          return socialHistory['recreationalDrugs'][index]['frequency'] == "0"||socialHistory['recreationalDrugs'][index]['type'] == ""?SizedBox(): Text(
                           'Patient uses ${socialHistory['recreationalDrugs'][index]['type']} ${socialHistoryUsages[int.parse(socialHistory['recreationalDrugs'][index]['frequency']) - 1]}.',
                         );
                       },
