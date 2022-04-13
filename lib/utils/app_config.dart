f7d5046cc8ab4c4bd952cceff5543228cf8a738f
amit <amit@appening.xyz>
2022-04-13 11:33:24 +0530

    apis changes

--- lib/utils/app_config.dart
+++ lib/utils/app_config.dart
@@ -22,10 +22,10 @@ void setEnvironment(Environment env) {
 
 dynamic get apiBaseUrl {
   // return "https://dev.hutano.com/";
-  return "https://staging.hutano.com/";
+  // return "https://staging.hutano.com/";
 
   // return "https://production.hutano.com/";
-  // return "https://www.hutano.com/";
+  return "https://www.hutano.com/";
 }
 
 dynamic get imageUrl {
